688f15c0746572e7443260ccdabdc47de63d9371
Catharina Satile <catsatile@gmail.com>
2026-04-21 12:56:58 -0300

    correção de organização de pastas

--- nodus-app/src/app/app.routes.ts
+++ nodus-app/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Header } from './components/header/header';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'navbar', component: Navbar },
+    {path: 'header', component: Header},
+    {path: 'login', component: Login}
+];
