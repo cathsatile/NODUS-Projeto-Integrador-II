80b74e8010b4b178144f9aff995b9f72c27dc21d
Catharina Satile <catsatile@gmail.com>
2026-05-12 21:23:40 -0300

    Tela de sessões e pop ups de cadastro de sessões e pacientes

--- nodus-app/src/app/pages/home-page/home-page.ts
+++ nodus-app/src/app/pages/home-page/home-page.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
+import { AddSectionPaciente } from '../../components/add-section-paciente/add-section-paciente';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [AddSectionPaciente, MatDialogModule],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePage {
-  psiNome =  'DR. Fulano';
+  psiNome = 'DR. Fulano';
   sessaoHoje = 'Nenhuma sessão hoje';
   numPacientes = 3;
   numSessoesMes = 10;
+  popupAberto = false;
+
+
+  constructor(private dialog: MatDialog,
+    private router: Router
+  ) { }
+
+  goToSections() {
+    this.router.navigate(['/principal/sections']);
+  }
+
+  openModal(modal: string): void {
+
+    this.dialog.open(AddSectionPaciente, {
+
+      data: {
+        add: modal
+      },
+
+      width: 'auto',
+      maxWidth: '95vw',
+
+      position: {
+        bottom: '0'
+      },
+
+      panelClass: 'bottom-modal'
+    });
+
+  }
 }
